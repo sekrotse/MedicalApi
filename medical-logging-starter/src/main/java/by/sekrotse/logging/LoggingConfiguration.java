7330bc685dc2968fc5d8023a2ced808b05aeafff
Кротов Сергей <s.krotau@bpcbt.com>
2022-10-20 17:36:37 +0300

    Create logging starter

---
+++ medical-logging-starter/src/main/java/by/sekrotse/logging/LoggingConfiguration.java
@@ -0,0 +1,7 @@
+package by.sekrotse.logging;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LoggingConfiguration {
+}
---
+++ medical-logging-starter/src/test/java/by/sekrotse/logging/MedicalLoggingStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package by.sekrotse.logging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalLoggingStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
